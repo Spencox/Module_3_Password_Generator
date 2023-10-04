2896cd57836e800cd8009a51b5e0200d8d933c0c
Spencer Cox <spencox@gmail.com>
2023-10-04 13:36:51 -0500

    got additional error checking and code cleanup in place

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,6 @@ const LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
 const UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const NUMBERS = "0123456789"
 
-
 // Helper Function to randomize values
 function randomizer(array){
     return array[(Math.floor(Math.random() * array.length))];
@@ -19,7 +18,7 @@ let pwdLength = () => {
     if(Number.isFinite(userInputLength) && (userInputLength >= 8) && (userInputLength <= 128)){
       return userInputLength
     }else{
-      userInputLength = Number(prompt("You did not enter a number, re-enter password length 8 through 128 characters:"))
+      userInputLength = Number(prompt("You did not enter a number or a number outside the range, re-enter password length 8 through 128 characters:"))
     }
   }
 }
@@ -29,17 +28,17 @@ function generatePassword() {
   let length = pwdLength();
   let password = []
   let passwordComponents = []
-  console.log("User Chose: " + length);
   let upperCase = confirm("Would you like to use uppercase letters in password? \n(OK for Yes Cancel for No)");
-  console.log("Uppercase: " + upperCase);
   let lowerCase = confirm("Would you like to use lowercase letters in password? \n(OK for Yes Cancel for No) ");
-  console.log("Lowercase: " + lowerCase);
   let numbers = confirm("Would you like to use numbers in password? \n(OK for Yes Cancel for No) ");
-  console.log("Numbers: " + numbers);
   let specials = confirm("Would you like to use special characters in password? \n(OK for Yes Cancel for No) ");
-  console.log("Special Characters: " + specials);
   
-  // Assign User Choices to array
+  // Make sure user selected at least one character type
+  if (!upperCase && !lowerCase && !numbers && !specials) {
+    alert("You did not choose any character types. Try Again")
+    return "Try Again";
+  }
+  // Assign user choices to array
   if (upperCase === true) {
     passwordComponents.push(UPPER_CASE)
   }
@@ -55,9 +54,7 @@ function generatePassword() {
   
   // Generate password randomly
   for(let i = 0; i < length; ++i){
-    let components = randomizer(passwordComponents);
-    console.log("Sending: " + components);
-    password += randomizer(components);
+    password += randomizer(randomizer(passwordComponents));
   }
   return password;
 }
@@ -67,9 +64,9 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
